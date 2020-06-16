ae5715199ec52f1cf90a3b13bd80d24344bb08b3
26kimayam <26kimayam@gmail.com>
2020-06-16 18:19:29 +0000

    made screen appear and downloaded the fish image.

--- main.py
+++ main.py
@@ -1,4 +1,25 @@
-import pygame.examples.aliens as game
+# import pygame.examples.aliens as game
+# game.main()
+import pygame
+from pygame.locals import *
+import sys
 
-game.main()
-print("hello fishies")
+pygame.init()
+screen_info = pygame.display.Info()
+
+#set the width and height of the screen
+screen_size = (width, height) = (int(screen_info.current_w), int(screen_info.current_h))
+screen = pygame.display.set_mode(screen_size)
+clock = pygame.time.Clock()
+
+
+color = (0, 127, 255)
+
+def main():
+  while True:
+    clock.tick(60)
+    screen.fill(color)
+    pygame.display.flip()
+
+if __name__ == '__main__':
+  main()
